bee47bb17d718e551d52fba0016d9ca07517ee78
Indresh Yadav <147027546+IndreshYadav003@users.noreply.github.com>
2024-01-03 02:24:48 +0530

    Add files via upload

---
+++ find data type of var.js
@@ -0,0 +1,10 @@
+var a=1;
+var b=2.0;
+var c="indresh yadav";
+var array=[1,2,3,4];
+console.log(typeof a);//number
+console.log(typeof b);//number
+console.log(typeof c);//string
+console.log(typeof array);//object
+
+
