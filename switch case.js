42bf367dc7fe91609cbccd4f243ea80202843a57
Indresh Yadav <147027546+IndreshYadav003@users.noreply.github.com>
2024-01-05 08:11:29 +0530

    Add files via upload

---
+++ switch case.js
@@ -0,0 +1,17 @@
+var day=65;
+switch(day){
+    case 1:console.log("Sunday");
+    break;
+    case 2:console.log("Monday");
+    break;
+    case 3:console.log("Tuesday");
+    break;
+    case 4:console.log("Wensday");
+    break;
+    case 5:console.log("Friday");
+    break;
+    case 6:console.log("Befor Sunday");
+    break;
+    default:console.log("Day is not exots");
+
+}
