ffa6e9ffdfbd85a6e8ab87ee855cadf2c12e8fdb
Indresh Yadav <147027546+IndreshYadav003@users.noreply.github.com>
2024-01-05 08:11:00 +0530

    Add files via upload

---
+++ Nested condition.js
@@ -0,0 +1,16 @@
+var a=123;
+var b=5455;
+var c=987;
+//find greater number
+if(a>b){
+    if(a>c){
+        console.log("A is greater");
+    }else {
+        console.log(" C is greater");
+    }
+}else if(b>c){
+    console.log("B is greater");
+}
+else{
+    console.log("c is greater");
+}
