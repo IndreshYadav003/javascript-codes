129b7c55e97fb7c3848ec12359678f36b2ffee3a
Indresh Yadav <147027546+IndreshYadav003@users.noreply.github.com>
2024-01-18 03:32:04 +0530

    Add files via upload

---
+++ accesvalue.js
@@ -0,0 +1,20 @@
+const person={
+    name:"kgr",
+    "firstname":"Indresh ahir",
+    lastname:'Yadav',
+    city:"basti",
+    age:15,
+}
+//erroe so
+//console.log(person."firstname")
+console.log(person.lastname)
+console.log(person["firstname"])
+console.log(person.firstname)
+
+/**
+ * output of my code
+ * Yadav
+Indresh ahir
+Indresh ahir
+
+ */
