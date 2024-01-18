4aa8fafdc486d7cdc77e7d9aefa02cd2710be816
Indresh Yadav <147027546+IndreshYadav003@users.noreply.github.com>
2024-01-18 18:47:42 +0530

    basic method of math

---
+++ mathMethods.js
@@ -0,0 +1,25 @@
+//pi fuction
+console.log(Math.PI)//3.141592653589793
+//Absolute method
+console.log(Math.abs(-97));//97
+//ceiling method
+console.log(Math.ceil(65.98));//66
+//floor method
+console.log(Math.floor(65.987));//65
+//exponent method
+console.log(Math.exp(4));//54.598150033144236
+//random methodth
+console.log(Math.random())//0.3706844873164101
+//method round
+console.log(Math.round(1,5 ))//1
+//power method
+console.log(Math.pow(4,3))//64
+// max method
+console.log(Math.max(35,35,66,55,66))//66
+//min method
+console.log(Math.min(3,1,45,5,4,34,56))//1
+//squretroot method
+console.log(Math.sqrt(64))//8
+//trunch method
+console.log(Math.trunc())//NaN
+console.log(Math.trunc(23.3431))//23
